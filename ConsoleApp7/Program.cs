845d9b361b4d12294d7f12d24b416de5b0461de5
pankajdaundkar <phd27993@gmail.com>
2023-06-12 10:16:41 +0530

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Schema;
+
+int marathi, hindi, english, total, perc;
+
+Console.WriteLine("Enter marathi marks");
+
+marathi = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Entre hindi marks");
+
+hindi = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Entre english marks");
+
+english = Convert.ToInt32(Console.ReadLine());
+
+total = marathi + hindi + english;
+Console.WriteLine($"total of three subject is ={total}");
+
+perc = (marathi + hindi + english) / 3;
+Console.WriteLine($"percentage of three sub is = {perc}");
